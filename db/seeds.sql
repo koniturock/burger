b20d9ed8dc5e380dc4ffafa2973b8bbeabea75e9
Koni Turock <koniturock@Konis-MacBook-Air.local>
2017-07-12 21:30:45 -0400

    updated

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger', FALSE);
+ INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+ INSERT INTO burgers (burger_name, devoured) VALUES ('Steak Burger', FALSE);
